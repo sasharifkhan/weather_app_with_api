ad18d2785d66538d99958fcf0205c2fc891b8458
SA SHARIF KHAN <sharifbmdd@gmail.com>
2025-10-21 12:56:26 +0600

    Update

--- lib/api_calling.dart
+++ lib/api_calling.dart
@@ -4,8 +4,9 @@ import 'package:http/http.dart' as http ;
 
 class ApiCalling {
 
-    gettempC_logation()async {
-      final response = await http.get(Uri.parse('http://api.weatherapi.com/v1/current.json?key=72e7e4f689134be0b3640336252309&q=Bangladesh'));
+    getweatherDetails()async {
+      String q = 'Dhaka';
+      final response = await http.get(Uri.parse('http://api.weatherapi.com/v1/current.json?key=72e7e4f689134be0b3640336252309&q=$q'));
       final jsonData = jsonDecode(response.body);
       final tempC=  jsonData['current']['temp_c'].toString();
       final location=  jsonData['location']['name'].toString();
