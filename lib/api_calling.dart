08a76205edef448bd8df50233dacc12c35c7cd76
SA SHARIF KHAN <sharifbmdd@gmail.com>
2025-10-21 12:57:54 +0600

    Update

--- lib/api_calling.dart
+++ lib/api_calling.dart
@@ -7,23 +7,28 @@ class ApiCalling {
     getweatherDetails()async {
       String q = 'Dhaka';
       final response = await http.get(Uri.parse('http://api.weatherapi.com/v1/current.json?key=72e7e4f689134be0b3640336252309&q=$q'));
-      final jsonData = jsonDecode(response.body);
-      final tempC=  jsonData['current']['temp_c'].toString();
-      final location=  jsonData['location']['name'].toString();
-      final localTime=  jsonData['location']['localtime'].toString();
-      final tempIcon=  jsonData['current']['condition']['icon'].toString();
-      final tempText=  jsonData['current']['condition']['text'].toString();
-      final windKmph=  jsonData['current']['wind_kph'].toString();
-      final humidity=  jsonData['current']['humidity'].toString();
-      final feelslikeTempC=  jsonData['current']['feelslike_c'].toString();
-      print('tempC: $tempC');
-      print('location: $location');
-      print('localTime: $localTime');
-      print('tempicon: $tempIcon');
-      print('tempText $tempText');
-      print('windkmph: $windKmph');
-      print('humidity: $humidity');
-      print('feelslikeTempc: $feelslikeTempC');
+      if (response.statusCode==200){
+        final jsonData = jsonDecode(response.body);
+        final tempC=  jsonData['current']['temp_c'].toString();
+        final location=  jsonData['location']['name'].toString();
+        final localTime=  jsonData['location']['localtime'].toString();
+        final tempIcon=  jsonData['current']['condition']['icon'].toString();
+        final tempText=  jsonData['current']['condition']['text'].toString();
+        final windKmph=  jsonData['current']['wind_kph'].toString();
+        final humidity=  jsonData['current']['humidity'].toString();
+        final feelslikeTempC=  jsonData['current']['feelslike_c'].toString();
+        print('tempC: $tempC');
+        print('location: $location');
+        print('localTime: $localTime');
+        print('tempicon: $tempIcon');
+        print('tempText $tempText');
+        print('windkmph: $windKmph');
+        print('humidity: $humidity');
+        print('feelslikeTempc: $feelslikeTempC');
+      } else{
+        print('Error');
+      }
+
 
     }
 
